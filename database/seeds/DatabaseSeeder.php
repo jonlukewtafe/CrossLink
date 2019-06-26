2f72add2fe5e50a02ca592af669b41ead10d23ab
Jon-Luke Willemsen <36464563+jonlukewtafe@users.noreply.github.com>
2019-06-26 13:20:13 +0800

    .

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TagsSeeder::class);
         $this->call(BookmarksSeeder::class);
         $this->call(UserBookmarkSeeder::class);
+        $this->call(TagBookmarkSeeder::class);
     }
 }
--- database/seeds/TagBookmarkSeeder.php
+++ database/seeds/TagBookmarkSeeder.php
@@ -2,39 +2,39 @@
 
 use Illuminate\Database\Seeder;
 
-class TagsSeeder extends Seeder
+class TagBookmarkSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-
-
     public function run()
     {
-        DB::table('tags')->insert([
-            'name' => 'JavaScript',
+        DB::table('tag_bookmark')->insert([
+            'bookmark_id' => 1,
+            'tag_id' => 1,
             'created_at' => DB::raw('CURRENT_TIMESTAMP'),
             'updated_at' => DB::raw('CURRENT_TIMESTAMP'),
         ]);
-        DB::table('tags')->insert([
-            'name' => 'Java',
+
+        DB::table('tag_bookmark')->insert([
+            'bookmark_id' => 1,
+            'tag_id' => 5,
             'created_at' => DB::raw('CURRENT_TIMESTAMP'),
             'updated_at' => DB::raw('CURRENT_TIMESTAMP'),
         ]);
-        DB::table('tags')->insert([
-            'name' => 'C#',
+
+        DB::table('tag_bookmark')->insert([
+            'bookmark_id' => 2,
+            'tag_id' => 4,
             'created_at' => DB::raw('CURRENT_TIMESTAMP'),
             'updated_at' => DB::raw('CURRENT_TIMESTAMP'),
         ]);
-        DB::table('tags')->insert([
-            'name' => 'Laravel',
-            'created_at' => DB::raw('CURRENT_TIMESTAMP'),
-            'updated_at' => DB::raw('CURRENT_TIMESTAMP'),
-        ]);
-        DB::table('tags')->insert([
-            'name' => 'Test',
+
+        DB::table('tag_bookmark')->insert([
+            'bookmark_id' => 2,
+            'tag_id' => 5,
             'created_at' => DB::raw('CURRENT_TIMESTAMP'),
             'updated_at' => DB::raw('CURRENT_TIMESTAMP'),
         ]);
